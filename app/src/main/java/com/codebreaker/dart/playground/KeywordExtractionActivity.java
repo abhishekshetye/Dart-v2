9844f50db69015ec0e76a7730191c0c8318afafe
abhishek <abhi15kk@gmail.com>
2017-01-22 17:59:40 +0530

    Added Exude to remove Stopping words

--- app/src/main/java/com/codebreaker/dart/playground/KeywordExtractionActivity.java
+++ app/src/main/java/com/codebreaker/dart/playground/KeywordExtractionActivity.java
@@ -7,6 +7,8 @@ import android.util.Log;
 import com.codebreaker.dart.R;
 import com.codebreaker.dart.nlp.Extract;
 import com.codebreaker.dart.nlp.Keyword;
+import com.uttesh.exude.ExudeData;
+import com.uttesh.exude.exception.InvalidDataException;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -15,15 +17,20 @@ import java.util.List;
 public class KeywordExtractionActivity extends AppCompatActivity {
 
     List<Keyword> keywords = new ArrayList<>();
+    String mess = "Kendall has small legs. Kendall has small hands. The developers of java were good. They developed handing apps";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_keyword_extraction);
 
         try {
-            keywords = Extract.guessFromString("Kendall has small tits. She has small boobs. The developers of java were good. They developed breasts apps");
+            String excuded = ExudeData.getInstance().filterStoppingsKeepDuplicates(mess);
+            Log.d("KEYWORD", excuded);
+            keywords = Extract.guessFromString(excuded);
             Log.d("KEYWORD", keywords.size() + " ");
         } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InvalidDataException e) {
             e.printStackTrace();
         }
 
